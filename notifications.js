9131e07b1de312f97280ea6a287c2c81504736b1
MashaZoom <maryia.lyubomyrskyy@zoominfo.com>
2018-06-20 15:33:33 -0400

    changed

--- notifications.js
+++ notifications.js
@@ -1,25 +1,33 @@
-// var Client = require('coinbase').Client;
-// var client = new Client({'apiKey': '38NF0D7sLyZsnXEx',
-//                          'apiSecret': 'AauNuFudPvlLldhafci6l0GalNsW8UCt'});
-//
-// client.getNotifications(null, function(err, notifications) {
-//   console.log('notification: ', notifications);
-//   console.log('error: ',err);
-// });
-
+var express = require('express');
+var app = express();
 
 var Client = require('coinbase').Client;
 
 var client = new Client({'apiKey': '38NF0D7sLyZsnXEx',
                          'apiSecret': 'AauNuFudPvlLldhafci6l0GalNsW8UCt'});
 
-client.getAccount('da98fb1d-c7f1-563a-941d-ee9c02fd7b6e', function(err, account) {
-  account.getTransactions(null,function(err, txs) {
-    console.log(txs);
-  });
+app.post("/getCoinbaseTransaction", getCoinbaseTransaction);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
+//const router = require('express').Router();
 
-// client.getAccounts({}, function(err, accounts) {
-//   console.log(accounts);
-// });
+
+//router.post("/getCoinbaseTransaction", getCoinbaseTransaction);
+
+function getCoinbaseTransaction(req, res){
+
+  res.resolveAndResponse(getTransaction);
+
+}
+
+function getTransaction(){
+  return client.getAccount('da98fb1d-c7f1-563a-941d-ee9c02fd7b6e', function(err, account) {
+    return account.getTransactions(null,function(err, txs) {
+      console.log(txs);
+      return txt;
+    });
+  });
+}

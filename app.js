8ad9472b2eda813d67c90b05423738e92cd24333
MashaZoom <maryia.lyubomyrskyy@zoominfo.com>
2018-06-20 16:52:57 -0400

    test

--- app.js
+++ app.js
@@ -26,15 +26,44 @@ function getCoinbaseTransaction(req, res){
 
 }
 
+const timeInterval = 3000000000;
+
 function getTransaction(){
   return client.getAccount('da98fb1d-c7f1-563a-941d-ee9c02fd7b6e', function(err, account) {
-    return account.getTransactions(null,function(err, transaction) {
-      console.log(transaction);
-      if(transaction){
-        return transaction;
-      } else {
-        {message: 'there is an error, does not work!'};
+    return account.getTransactions(null,function(err, txs) {
+      console.log(txs);
+      // if(transaction){
+      //   return transaction;
+      // } else {
+      //   {message: 'there is an error, does not work!'};
+      // }
+
+      txs.sort(function(a, b){
+        return new Date(b.created_at) - new Date(a.created_at);
+      });
+
+      let tran = txs[0];
+      //console.log(tran);
+      let curDate = new Date();
+      let latestTransactionDate = new Date(txs[0].created_at);
+      var diff = curDate.getTime() - latestTransactionDate.getTime();
+      //console.log(diff);
+      var result = {
+        "status_code":400
       }
+      if (diff <= timeInterval) {
+        if (tran.status === 'completed' || tran.status === 'pending'){
+          var result = {
+            "status_code":200,
+            "amount": tran.amount,
+            "created_at":tran.created_at,
+            "status":tran.status,
+            "usd":tran.native_amount,
+          }
+        }
+      }
+      console.log(result);
+      return result;
 
 
     });

de75566e1d0b973d8b634edb9ccd86c40d3b9d6e
MashaZoom <maryia.lyubomyrskyy@zoominfo.com>
2018-06-21 09:08:14 -0400

    Nicks fix

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 const express = require('express')
 
 const app = express()
@@ -22,52 +21,54 @@ function getCoinbaseTransaction(req, res){
 
   console.log('this is working');
 
-  res.send(getTransaction());
+  getTransaction().then(function(result) {
+    console.log("result :: ", result);
+    res.send(result);
+  });
 
 }
 
 const timeInterval = 3000000000;
 
 function getTransaction(){
-  return client.getAccount('da98fb1d-c7f1-563a-941d-ee9c02fd7b6e', function(err, account) {
-    return account.getTransactions(null,function(err, txs) {
-      console.log(txs);
-      // if(transaction){
-      //   return transaction;
-      // } else {
-      //   {message: 'there is an error, does not work!'};
-      // }
 
-      txs.sort(function(a, b){
-        return new Date(b.created_at) - new Date(a.created_at);
+  var promise = new Promise(function(resolve,reject) {
+      return client.getAccount('da98fb1d-c7f1-563a-941d-ee9c02fd7b6e', function(err, account) {
+          return account.getTransactions(null,function(err, txs) {
+              console.log(txs);
+
+              txs.sort(function(a, b){
+                  return new Date(b.created_at) - new Date(a.created_at);
+              });
+
+              let tran = txs[0];
+              //console.log(tran);
+              let curDate = new Date();
+              let latestTransactionDate = new Date(txs[0].created_at);
+              var diff = curDate.getTime() - latestTransactionDate.getTime();
+              //console.log(diff);
+              var result = {
+                  "status_code":400
+              }
+              //if (diff <= timeInterval) {
+                  if (tran.status === 'completed' || tran.status === 'pending'){
+                      var result = {
+                          "status_code":200,
+                          "amount": tran.amount,
+                          "created_at":tran.created_at,
+                          "status":tran.status,
+                          "usd":tran.native_amount,
+                      }
+                  }
+              //}
+              return resolve(result);
+
+
+          });
       });
-
-      let tran = txs[0];
-      //console.log(tran);
-      let curDate = new Date();
-      let latestTransactionDate = new Date(txs[0].created_at);
-      var diff = curDate.getTime() - latestTransactionDate.getTime();
-      //console.log(diff);
-      var result = {
-        "status_code":400
-      }
-      if (diff <= timeInterval) {
-        if (tran.status === 'completed' || tran.status === 'pending'){
-          var result = {
-            "status_code":200,
-            "amount": tran.amount,
-            "created_at":tran.created_at,
-            "status":tran.status,
-            "usd":tran.native_amount,
-          }
-        }
-      }
-      console.log(result);
-      return result;
-
-
-    });
   });
+
+  return promise;
 }
 
 app.use('/', router);
